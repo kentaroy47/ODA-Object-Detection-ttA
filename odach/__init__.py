b351f2b9706c20186109fb98ce423a80bfe9c220
Kentaro Yoshioka <kentaroy@stanford.edu>
2025-08-18 10:38:33 +0900

    update for pypi

--- odach/__init__.py
+++ odach/__init__.py
@@ -1,1 +1,19 @@
+"""
+ODAch: An Object Detection TTA tool for Pytorch
+
+ODAch is a test-time-augmentation (TTA) tool for 2d object detectors
+with support for YOLO models from Ultralytics.
+
+Features:
+- Test Time Augmentation for object detection models
+- YOLO integration (YOLOv5, YOLOv8, YOLOv9)
+- Multiple augmentation strategies
+- Multi-scale TTA support
+- NMS and WBF integration
+"""
+
+__version__ = "0.2.0"
+__author__ = "Kentaro Yoshioka"
+__email__ = "meathouse47@gmail.com"
+
 from .oda import *
--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
-import os, sys
-import shutil
-import datetime
-
 from setuptools import setup, find_packages
-from setuptools.command.install import install
 
-readme = open('README.md').read()
+# Read README for long description
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
 
-VERSION = '0.1.5'
+# Read requirements
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = [line.strip() for line in f if line.strip() and not line.startswith('#')]
 
-# import subprocess
-# commit_hash = subprocess.check_output("git rev-parse HEAD", shell=True).decode('UTF-8').rstrip()
-# VERSION += "_" + str(int(commit_hash, 16))[:8]
-VERSION += "_" + datetime.datetime.now().strftime('%Y%m%d%H%M')[2:]
-# print(VERSION)
+VERSION = '0.2.0'
 
 setup(
     # Metadata
@@ -23,20 +18,43 @@ setup(
     author='Kentaro Yoshioka',
     author_email='meathouse47@gmail.com',
     url='https://github.com/kentaroy47/ODA-Object-Detection-ttA',
-    description='ODAch is a test-time-augmentation tool for pytorch 2d object detectors.',
-    long_description=readme,
+    description='ODAch is a test-time-augmentation tool for pytorch 2d object detectors with YOLO support.',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     license='MIT',
+    keywords='object-detection, pytorch, tta, test-time-augmentation, yolo, computer-vision',
 
     # Package info
-    packages=find_packages(exclude=('*test*',)),
+    packages=find_packages(exclude=('tests*', 'test_*', '*test*')),
+    include_package_data=True,
+    zip_safe=False,
 
-    #
-    zip_safe=True,
-    #nstall_requires=requirements,
+    # Dependencies
+    install_requires=requirements,
+    python_requires='>=3.7',
 
     # Classifiers
     classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Image Processing',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+
+    # Project URLs
+    project_urls={
+        'Bug Reports': 'https://github.com/kentaroy47/ODA-Object-Detection-ttA/issues',
+        'Source': 'https://github.com/kentaroy47/ODA-Object-Detection-ttA',
+        'Documentation': 'https://github.com/kentaroy47/ODA-Object-Detection-ttA#readme',
+    },
 )
